718f1dee4f64dfd53b8770b5eec839a834092e0a
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-01-16 19:31:36 +0530

    unncessry require

--- src/app.js
+++ src/app.js
@@ -1,75 +1,71 @@
-const path = require("path");
-const express = require("express");
-const hbs = require("hbs");
-const forecast = require("./utils/forecast");
-const geocode = require("./utils/geocode");
-const { send } = require("process");
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
 const port = process.env.PORT || 3000
-const app = express();
+const app = express()
 
+const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
 
-const publicPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
-const partialPath = path.join(__dirname, '../templates/partials');
+hbs.registerPartials(partialPath)
+app.use(express.static(publicPath))
 
-hbs.registerPartials(partialPath);
-app.use(express.static(publicPath));
-
-
-app.set('view engine', 'hbs');
-app.set("views", viewsPath);
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 
 app.get('', (req, res) => {
-    res.render("index", {
-        title: "Dark Sky ",
-        name: "Abhinav Jha"
-    });
+ res.render('index', {
+  title: 'Dark Sky ',
+  name: 'Abhinav Jha',
+ })
 })
 
 app.get('/help', (req, res) => {
-    res.render("help", {
-        title: "Dark Sky",
-        help: "Hover Here",
-        name: "Abhinav Jha"
-    });
+ res.render('help', {
+  title: 'Dark Sky',
+  help: 'Hover Here',
+  name: 'Abhinav Jha',
+ })
 })
 app.get('/help/*', (req, res) => {
-    res.render("Error", {
-        data: "Help Page not found"
-    })
-});
+ res.render('Error', {
+  data: 'Help Page not found',
+ })
+})
 
 app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: "You Must Provide Address ",
-        });
+ if (!req.query.address) {
+  return res.send({
+   error: 'You Must Provide Address ',
+  })
+ }
+ forecast(req.query.address, (error, data) => {
+  const forecast_Data = data
+  if (!error) {
+   geocode(req.query.address, (error, data) => {
+    const geocode_Data = data
+    if (error) {
+     res.send(error)
+    } else {
+     res.send({
+      address: geocode_Data,
+      weather_details: forecast_Data,
+     })
     }
-    forecast(req.query.address, (error, data) => {
-        const forecast_Data = data;
-        if (!error) {
-            geocode(req.query.address, (error, data) => {
-                const geocode_Data = data;
-                if (error) {
-                    res.send(error);
-                } else {
-                    res.send({
-                        address: geocode_Data,
-                        weather_details: forecast_Data,
-                    });
-                }
-            });
-        } else {
-            return res.send({ error });
-        }
-    });
-
-});
+   })
+  } else {
+   return res.send({ error })
+  }
+ })
+})
 app.get('*', (req, res) => {
-    res.render("Error", {
-        data: "Error-404 Page Can't Be Found"
-    })
-});
+ res.render('Error', {
+  data: "Error-404 Page Can't Be Found",
+ })
+})
 app.listen(port, () => {
-    console.log("Server is up on port-" + port)
-});
+ console.log('Server is up on port-' + port)
+})
