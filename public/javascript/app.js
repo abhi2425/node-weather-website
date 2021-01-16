61f1fdc1e22fcc096fb5994520e4ce7765e21ad0
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-01-17 02:25:50 +0530

    object destructuring

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -26,66 +26,74 @@ const cell18 = document.querySelector('.cell-18')
 const result = document.querySelector('.result')
 
 const mymap = L.map('mapid').setView([0, 0], 1)
-L.tileLayer(
- 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
- {
-  attribution:
-   'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken:
-   'pk.eyJ1IjoiYWJoaWluMjQyNSIsImEiOiJja2JqNTQxcWowa215MnNteTdpd2FkYWwyIn0.IOQupMMNwtszul5Wfhc3XQ',
- },
-).addTo(mymap)
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+   attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+   maxZoom: 18,
+   id: 'mapbox/streets-v11',
+   tileSize: 512,
+   zoomOffset: -1,
+   accessToken:
+      'pk.eyJ1IjoiYWJoaWluMjQyNSIsImEiOiJja2JqNTQxcWowa215MnNteTdpd2FkYWwyIn0.IOQupMMNwtszul5Wfhc3XQ',
+}).addTo(mymap)
 
 weatherForm.addEventListener('submit', (e) => {
- e.preventDefault()
- const location = search.value
- messageOne.textContent = 'Loading...'
+   e.preventDefault()
+   const location = search.value
+   messageOne.textContent = 'Loading...'
 
- fetch('/weather?address=' + location).then((response) => {
-  response.json().then((data) => {
-   if (data.error) {
-    console.log(data.error)
-    messageOne.textContent = data.error
-   } else {
-    messageOne.textContent = ''
-    L.marker([data.address.latitude, data.address.longitude]).addTo(mymap)
-    mymap.setView([data.address.latitude, data.address.longitude], 8)
+   fetch('/weather?address=' + location).then((response) => {
+      response.json().then((data) => {
+         if (data.error) {
+            console.log(data.error)
+            messageOne.textContent = data.error
+         } else {
+            const {
+               weather_details: {
+                  temperature,
+                  wind_direction,
+                  wind_speed,
+                  humidity,
+                  local_time,
+                  climate,
+               },
+               address: { latitude, longitude, location },
+            } = data
+            messageOne.textContent = ''
+            L.marker([latitude, longitude]).addTo(mymap)
+            mymap.setView([latitude, longitude], 8)
 
-    result.style = 'background-color: #eee; display: block;'
-    columnHeading1.style = 'background-color: #111'
-    columnHeading1.textContent = 'DATA'
-    columnHeading2.textContent = 'VALUES'
-    cell1.textContent = 'Latitude'
-    cell2.textContent = data.address.latitude
+            result.style = 'background-color: #eee; display: block;'
+            columnHeading1.style = 'background-color: #111'
+            columnHeading1.textContent = 'DATA'
+            columnHeading2.textContent = 'VALUES'
+            cell1.textContent = 'Latitude'
+            cell2.textContent = latitude
 
-    cell3.textContent = 'Longitude'
-    cell4.textContent = data.address.longitude
+            cell3.textContent = 'Longitude'
+            cell4.textContent = longitude
 
-    cell5.textContent = 'Location'
-    cell6.textContent = data.address.location
+            cell5.textContent = 'Location'
+            cell6.textContent = location
 
-    cell7.textContent = 'Temperature'
-    cell8.textContent = data.weather_details.temperature
+            cell7.textContent = 'Temperature'
+            cell8.textContent = temperature
 
-    cell9.textContent = 'Wind Speed'
-    cell10.textContent = data.weather_details.wind_speed
+            cell9.textContent = 'Wind Speed'
+            cell10.textContent = wind_speed
 
-    cell11.textContent = 'Wind Direction'
-    cell12.textContent = data.weather_details.wind_direction
+            cell11.textContent = 'Wind Direction'
+            cell12.textContent = wind_direction
 
-    cell13.textContent = 'Humidity'
-    cell14.textContent = data.weather_details.humidity
+            cell13.textContent = 'Humidity'
+            cell14.textContent = humidity
 
-    cell15.textContent = 'Local Time'
-    cell16.textContent = data.weather_details.local_time
+            cell15.textContent = 'Local Time'
+            cell16.textContent = local_time
 
-    cell17.textContent = 'Climate'
-    cell18.textContent = data.weather_details.climate
-   }
-  })
- })
+            cell17.textContent = 'Climate'
+            cell18.textContent = climate
+         }
+      })
+   })
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,28 +3,33 @@ require('dotenv').config()
 const key = process.env.WEATHER_API_KEY
 
 function forecast(address, callback) {
- const url =
-  'https://api.weatherapi.com/v1/current.json?key=' +
-  key +
-  '&q=' +
-  address +
-  ' '
+   const url = 'https://api.weatherapi.com/v1/current.json?key=' + key + '&q=' + address + ' '
 
- request({ url, json: true }, (error, { body }) => {
-  if (error) {
-   callback('Unable to connect weather services')
-  } else if (body.error) {
-   callback('Unable To Find Location')
-  } else {
-   callback(undefined, {
-    temperature: body.current.temp_c + '-C',
-    wind_speed: body.current.wind_kph + '-Kmph',
-    wind_direction: body.current.wind_dir,
-    humidity: body.current.humidity,
-    local_time: body.location.localtime,
-    climate: body.current.condition.text,
+   request({ url, json: true }, (error, { body }) => {
+      if (error) {
+         callback('Unable to connect weather services')
+      } else if (body.error) {
+         callback('Unable To Find Location')
+      } else {
+         const {
+            current: {
+               temp_c,
+               wind_kph,
+               wind_dir,
+               humidity,
+               condition: { text },
+            },
+            location: { localtime },
+         } = body
+         callback(undefined, {
+            temperature: temp_c + '-C',
+            wind_speed: wind_kph + '-Kmph',
+            wind_direction: wind_dir,
+            humidity: humidity,
+            local_time: localtime,
+            climate: text,
+         })
+      }
    })
-  }
- })
 }
 module.exports = forecast
