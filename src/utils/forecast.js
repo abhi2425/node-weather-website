eecb2a1e62f5cc2f953c9dd387b51b77f5176fd0
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-01-16 19:15:06 +0530

    syntax check

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,26 +1,30 @@
-const request = require("request");
+const request = require('request')
 require('dotenv').config()
 const key = process.env.WEATHER_API_KEY
 
 function forecast(address, callback) {
-    const url = "https://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + address + " ";
+ const url =
+  'https://api.weatherapi.com/v1/current.json?key=' +
+  key +
+  '&q=' +
+  address +
+  ' '
 
-    request({ url, json: true }, (error, { body } = {}) => {
-        if (error) {
-            callback("Unable to connect weather services");
-        } else if (body.error) {
-            callback("Unable To Find Location");
-        } else {
-            callback(undefined, {
-                temperature: body.current.temp_c + "-C",
-                wind_speed: body.current.wind_kph + "-Kmph",
-                wind_direction: body.current.wind_dir,
-                humidity: body.current.humidity,
-                local_time: body.location.localtime,
-                climate: body.current.condition.text
-
-            })
-        }
-    });
+ request({ url, json: true }, (error, { body }) => {
+  if (error) {
+   callback('Unable to connect weather services')
+  } else if (body.error) {
+   callback('Unable To Find Location')
+  } else {
+   callback(undefined, {
+    temperature: body.current.temp_c + '-C',
+    wind_speed: body.current.wind_kph + '-Kmph',
+    wind_direction: body.current.wind_dir,
+    humidity: body.current.humidity,
+    local_time: body.location.localtime,
+    climate: body.current.condition.text,
+   })
+  }
+ })
 }
-module.exports = forecast;
+module.exports = forecast
